1bba68615c483e9987a52af7d53b8556f036b6ed
Pauline Moraa <pmoraa431@gmail.com>
2025-04-11 13:36:44 +0300

    Create components that return JSX Use props to make components dynamic Transform lists of data into lists of components

---
+++ src/components/Article.js
@@ -0,0 +1,25 @@
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+    function renderMinutesToRead() {
+      if (!minutes) return null;
+      
+      if (minutes < 30) {
+        const coffeeCups = Math.ceil(minutes / 5);
+        return `${'☕️'.repeat(coffeeCups)} ${minutes} min read`;
+      } else {
+        const bentoBoxes = Math.ceil(minutes / 10);
+        return `${'🍱'.repeat(bentoBoxes)} ${minutes} min read`;
+      }
+    }
+  
+    return (
+      <article>
+        <h3>{title}</h3>
+        <small>
+          {date} • {renderMinutesToRead()}
+        </small>
+        <p>{preview}</p>
+      </article>
+    );
+  }
+  
+  export default Article;
